eee9edf51d2892a5727f607ba8e28e61c7253950
Wiktor Krzyżanowski <wiktor.krzyzanowski1@gmail.com>
2019-01-18 14:20:29 +0100

    Upload images and pdf when not exceed limit of 100MB

--- src/main/java/pl/wiktor/TestFileUploadRest/ConfigClass.java
+++ src/main/java/pl/wiktor/TestFileUploadRest/ConfigClass.java
@@ -1,13 +1,13 @@
 package pl.wiktor.TestFileUploadRest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.EnvironmentAware;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
+@EnableWebMvc
 public class ConfigClass extends WebMvcConfigurationSupport {
 
     @Autowired
@@ -17,5 +17,15 @@ public class ConfigClass extends WebMvcConfigurationSupport {
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         String DOWNLOAD_DIRECTORY = environment.getProperty("download.file.directory");
         registry.addResourceHandler("/srv_images/**").addResourceLocations(DOWNLOAD_DIRECTORY);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
     }
 }
---
+++ src/main/java/pl/wiktor/TestFileUploadRest/ServletInitializer.java
@@ -0,0 +1,13 @@
+package pl.wiktor.TestFileUploadRest;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TestFileUploadRestApplication.class);
+    }
+
+}
